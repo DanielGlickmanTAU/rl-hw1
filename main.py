89399ed708264c6e5ce08dee86b38e28e35dc9d1
Glickman <glickman1@mail.tau.ac.il>
2021-03-26 10:25:59 +0300

    measure total reward

--- main.py
+++ main.py
@@ -2,14 +2,26 @@ import gym
 
 import numpy
 
+should_render = True
+
 weights = numpy.random.uniform(low=-1., high=1., size=4)
 env = gym.make('CartPole-v0')
 env.reset()
-#todo check first obser
+# todo check first obser
 action = 0
-for _ in range(1000):
-    env.render()
+total_reward = 0.
+for _ in range(200):
+    if should_render:
+        env.render()
+
     obs, reward, done, info = env.step(action)  # take a random action
+    total_reward += reward
     action = 1 if (weights @ obs) > 0 else 0
-    print(done)
+    if done and reward:
+        print('WTF')
+    if not done and not reward:
+        print('????')
+    if done:
+        break
 env.close()
+print(total_reward)
