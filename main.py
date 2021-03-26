d21773c41959b71691941f4e5693e8d99c8611ec
Glickman <glickman1@mail.tau.ac.il>
2021-03-26 10:33:32 +0300

    extract episode loop

--- main.py
+++ main.py
@@ -2,26 +2,31 @@ import gym
 
 import numpy
 
-should_render = True
+should_render = False
 
-weights = numpy.random.uniform(low=-1., high=1., size=4)
 env = gym.make('CartPole-v0')
-env.reset()
-# todo check first obser
-action = 0
-total_reward = 0.
-for _ in range(200):
-    if should_render:
-        env.render()
 
-    obs, reward, done, info = env.step(action)  # take a random action
-    total_reward += reward
-    action = 1 if (weights @ obs) > 0 else 0
-    if done and reward:
-        print('WTF')
-    if not done and not reward:
-        print('????')
-    if done:
-        break
+
+def do_episode(weights):
+    env.reset()
+    # todo check first obser
+    action = 0
+    total_reward = 0.
+    for _ in range(200):
+        if should_render:
+            env.render()
+
+        obs, reward, done, info = env.step(action)  # take a random action
+        total_reward += reward
+        action = 1 if (weights @ obs) > 0 else 0
+        if done:
+            break
+
+    return total_reward
+
+
+# q
+weights = numpy.random.uniform(low=-1., high=1., size=4)
+print(do_episode(weights))
+
 env.close()
-print(total_reward)
